f98c3f758223912290017e5f6c6e81ccaf0ecd85
SamuelPiyu <samuelpiyu69@gmail.com>
2026-07-05 08:12:59 +0700

    menambahkan file vercel json dan memperbaiki routing

--- config/services.php
+++ config/services.php
@@ -21,7 +21,7 @@ return [
     'resend' => [
         'key' => env('RESEND_API_KEY'),
     ],
-
+    
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
